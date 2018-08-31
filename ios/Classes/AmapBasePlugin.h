a86da1384f2623018aeb350bc44c8680802584bd
yohom <382146139@qq.com>
2018-08-31 15:39:37 +0800

    迁移ios端的导航代码

--- ios/Classes/AmapBasePlugin.h
+++ ios/Classes/AmapBasePlugin.h
@@ -1,4 +1,10 @@
 #import <Flutter/Flutter.h>
+#import <AMapNaviKit/AMapNaviKit.h>
+#import <MAMapKit/MAMapKit.h>
+#import <AMapFoundationKit/AMapFoundationKit.h>
 
 @interface AmapBasePlugin : NSObject<FlutterPlugin>
+
+@property (nonatomic, strong) AMapNaviCompositeManager *compositeManager;
+
 @end
--- ios/Classes/AmapBasePlugin.m
+++ ios/Classes/AmapBasePlugin.m
@@ -1,20 +1,40 @@
 #import "AmapBasePlugin.h"
 
 @implementation AmapBasePlugin
+
+- (AMapNaviCompositeManager *)compositeManager {
+    if (!_compositeManager) {
+        _compositeManager = [[AMapNaviCompositeManager alloc] init];  // 初始化
+    }
+    return _compositeManager;
+}
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"amap_base"
-            binaryMessenger:[registrar messenger]];
-  AmapBasePlugin* instance = [[AmapBasePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"me.yohom/amap_navi"
+                                     binaryMessenger:[registrar messenger]];
+    AmapBasePlugin* instance = [[AmapBasePlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"startNavi" isEqualToString:call.method]) {
+        NSDictionary* paramDic = call.arguments;
+        CGFloat lat = [paramDic[@"lat"] doubleValue];
+        CGFloat lon = [paramDic[@"lon"] doubleValue];
+        AMapNaviCompositeUserConfig* config = [[AMapNaviCompositeUserConfig alloc] init];
+        [config setRoutePlanPOIType:AMapNaviRoutePlanPOITypeEnd
+                           location:[AMapNaviPoint locationWithLatitude:lat longitude:lon]
+                               name:@""
+                              POIId:nil];
+        [self.compositeManager presentRoutePlanViewControllerWithOptions:config];
+    } else if([@"setKey" isEqualToString:call.method]) {
+        NSString* key = call.arguments[@"key"];
+        [AMapServices sharedServices].apiKey = key;
+        result(@"key设置成功");
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
