796b2bd4f5f3601b31b60caae6524e521a72a4d3
yohom <382146139@qq.com>
2018-12-06 13:06:04 +0800

    fix #7

--- android/src/main/kotlin/me/yohom/amapbase/map/AMapFactory.kt
+++ android/src/main/kotlin/me/yohom/amapbase/map/AMapFactory.kt
@@ -1,7 +1,10 @@
 package me.yohom.amapbase.map
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Application
 import android.content.Context
+import android.os.Bundle
 import android.view.View
 import com.amap.api.maps.AMapOptions
 import com.amap.api.maps.CameraUpdateFactory
@@ -18,17 +21,20 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
-import me.yohom.amapbase.AMapBasePlugin
+import me.yohom.amapbase.*
+import me.yohom.amapbase.AMapBasePlugin.Companion.registrar
 import me.yohom.amapbase.map.model.*
 import me.yohom.amapbase.map.overlay.DrivingRouteOverlay
 import me.yohom.amapbase.utils.*
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
 const val mapChannelName = "me.yohom/map"
 const val markerClickedChannelName = "me.yohom/marker_clicked"
 const val success = "调用成功"
 
-class AMapFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class AMapFactory(private val activityState: AtomicInteger)
+    : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
 
     override fun create(context: Context, id: Int, params: Any?): PlatformView {
         checkPermission()
@@ -36,7 +42,7 @@ class AMapFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
         val options = Gson()
                 .fromJson(params as String, UnifiedAMapOptions::class.java)
 
-        val view = AMapView(context, id, options.toAMapOption())
+        val view = AMapView(context, id, activityState, options.toAMapOption())
         view.setup()
         return view
     }
@@ -45,26 +51,50 @@ class AMapFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
 @SuppressLint("CheckResult")
 class AMapView(private val context: Context,
                private val id: Int,
-               amapOptions: AMapOptions) : PlatformView {
+               private val activityState: AtomicInteger,
+               amapOptions: AMapOptions) : PlatformView , Application.ActivityLifecycleCallbacks{
 
     private val mapView = TextureMapView(context, amapOptions)
-    private var eventSink: EventChannel.EventSink? = null
+    private var disposed = false
+    private val registrarActivityHashCode: Int = AMapBasePlugin.registrar.activity().hashCode()
 
     override fun getView(): View = mapView
 
     override fun dispose() {
-        mapView.onPause()
+        if (disposed) {
+            return
+        }
+        disposed = true
         mapView.onDestroy()
+
+        registrar.activity().application.unregisterActivityLifecycleCallbacks(this)
     }
 
     fun setup() {
-        mapView.onCreate(null)
+        when (activityState.get()) {
+            STOPPED -> {
+                mapView.onCreate(null)
+                mapView.onResume()
+                mapView.onPause()
+            }
+            RESUMED -> {
+                mapView.onCreate(null)
+                mapView.onResume()
+            }
+            CREATED -> mapView.onCreate(null)
+            DESTROYED -> {
+            }
+            else -> throw IllegalArgumentException("Cannot interpret " + activityState.get() + " as an activity activityState")
+        }
 
+        // 设置method channel
         val mapChannel = MethodChannel(AMapBasePlugin.registrar.messenger(), "$mapChannelName$id")
         mapChannel.setMethodCallHandler { call, result ->
             handleMethodCall(call, result)
         }
 
+        // 设置marker click event channel
+        var eventSink: EventChannel.EventSink? = null
         val markerClickedEventChannel = EventChannel(AMapBasePlugin.registrar.messenger(), "$markerClickedChannelName$id")
         markerClickedEventChannel.setStreamHandler(object : EventChannel.StreamHandler {
             override fun onListen(p0: Any?, sink: EventChannel.EventSink?) {
@@ -77,6 +107,9 @@ class AMapView(private val context: Context,
             eventSink?.success(UnifiedMarkerOptions(it.options).toJson())
             true
         }
+
+        // 注册生命周期
+        registrar.activity().application.registerActivityLifecycleCallbacks(this)
     }
 
     private fun handleMethodCall(methodCall: MethodCall, methodResult: MethodChannel.Result) {
@@ -356,5 +389,52 @@ class AMapView(private val context: Context,
             }
             else -> methodResult.notImplemented()
         }
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle) {
+        if (disposed || activity.hashCode() != registrarActivityHashCode) {
+            return
+        }
+        mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        if (disposed || activity.hashCode() != registrarActivityHashCode) {
+            return
+        }
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        if (disposed || activity.hashCode() != registrarActivityHashCode) {
+            return
+        }
+        mapView.onResume()
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+        if (disposed || activity.hashCode() != registrarActivityHashCode) {
+            return
+        }
+        mapView.onPause()
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+        if (disposed || activity.hashCode() != registrarActivityHashCode) {
+            return
+        }
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        if (disposed || activity.hashCode() != registrarActivityHashCode) {
+            return
+        }
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        if (disposed || activity.hashCode() != registrarActivityHashCode) {
+            return
+        }
+        mapView.onDestroy()
     }
 }
