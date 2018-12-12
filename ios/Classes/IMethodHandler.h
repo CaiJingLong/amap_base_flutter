e50163abd9f38afd519321c960e8c7f7165b6ec9
yohom <382146139@qq.com>
2018-12-12 10:21:09 +0800

    enhancement: 补全架构 ios

--- ios/Classes/IMethodHandler.h
+++ ios/Classes/IMethodHandler.h
@@ -5,19 +5,35 @@
 #import <Foundation/Foundation.h>
 #import <Flutter/Flutter.h>
 
+//region 地图
 @class MAMapView;
 @class FlutterMethodCall;
 
 @protocol MapMethodHandler <NSObject>
-
 @required
 - (NSObject <MapMethodHandler> *)initWith:(MAMapView *)mapView;
 @required
 - (void)onMethodCall:(FlutterMethodCall *)call :(FlutterResult)result;
-
 @end
+//endregion
 
+//region 搜索
 @protocol SearchMethodHandler <NSObject>
 @required
 - (void)onMethodCall:(FlutterMethodCall *)call :(FlutterResult)result;
 @end
+//endregion
+
+//region 导航
+@protocol NaviMethodHandler <NSObject>
+@required
+- (void)onMethodCall:(FlutterMethodCall *)call :(FlutterResult)result;
+@end
+//endregion
+
+//region 定位
+@protocol LocationMethodHandler <NSObject>
+@required
+- (void)onMethodCall:(FlutterMethodCall *)call :(FlutterResult)result;
+@end
+//endregion
