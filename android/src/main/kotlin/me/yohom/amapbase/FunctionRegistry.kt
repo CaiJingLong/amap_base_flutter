c82c1857355a45ddeb7163441707bba637c80c8d
yohom <382146139@qq.com>
2018-12-13 21:47:35 +0800

    feature: iOS端定位隔离

--- android/src/main/kotlin/me/yohom/amapbase/FunctionRegistry.kt
+++ android/src/main/kotlin/me/yohom/amapbase/FunctionRegistry.kt
@@ -1,57 +1,21 @@
 package me.yohom.amapbase
 
-import me.yohom.amapbase.map.handler.calculatetool.ConvertCoordinate
-import me.yohom.amapbase.map.handler.createmap.*
-import me.yohom.amapbase.map.handler.draw.AddMarker
-import me.yohom.amapbase.map.handler.draw.AddMarkers
-import me.yohom.amapbase.map.handler.draw.AddPolyline
-import me.yohom.amapbase.map.handler.draw.ClearMarker
-import me.yohom.amapbase.map.handler.interact.*
-import me.yohom.amapbase.navi.handler.StartNavi
-import me.yohom.amapbase.search.handler.fetchdata.*
-import me.yohom.amapbase.search.handler.routeplan.CalculateDriveRoute
-
 /**
  * 地图功能集合
  */
 val MAP_METHOD_HANDLER: Map<String, MapMethodHandler> = mapOf(
-        "map#setMyLocationStyle" to SetMyLocationStyle,
-        "map#setUiSettings" to SetUiSettings,
-        "marker#addMarker" to AddMarker,
-        "marker#addMarkers" to AddMarkers,
-        "marker#clear" to ClearMarker,
-        "map#showIndoorMap" to ShowIndoorMap,
-        "map#setMapType" to SetMapType,
-        "map#setLanguage" to SetLanguage,
-        "map#clear" to ClearMap,
-        "map#setZoomLevel" to SetZoomLevel,
-        "map#setPosition" to SetPosition,
-        "map#setMapStatusLimits" to SetMapStatusLimits,
-        "tool#convertCoordinate" to ConvertCoordinate,
-        "offline#openOfflineManager" to OpenOfflineManager,
-        "map#addPolyline" to AddPolyline,
-        "map#zoomToSpan" to ZoomToSpan,
-        "map#changeLatLng" to ChangeLatLng
 )
 
 /**
  * 搜索功能集合
  */
 val SEARCH_METHOD_HANDLER: Map<String, SearchMethodHandler> = mapOf(
-        "search#calculateDriveRoute" to CalculateDriveRoute,
-        "search#searchPoi" to SearchPoiKeyword,
-        "search#searchPoiBound" to SearchPoiBound,
-        "search#searchPoiPolygon" to SearchPoiPolygon,
-        "search#searchPoiId" to SearchPoiId,
-        "search#searchRoutePoiLine" to SearchRoutePoiLine,
-        "search#searchRoutePoiPolygon" to SearchRoutePoiPolygon
 )
 
 /**
  * 导航功能集合
  */
 val NAVI_METHOD_HANDLER: Map<String, NaviMethodHandler> = mapOf(
-        "navi#startNavi" to StartNavi
 )
 
 /**
