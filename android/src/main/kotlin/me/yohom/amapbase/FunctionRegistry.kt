814c9405083f4bd1c84121d4af3c920a97b0355c
yohom <382146139@qq.com>
2018-12-13 21:34:34 +0800

    feature: Android端地图隔离

--- android/src/main/kotlin/me/yohom/amapbase/FunctionRegistry.kt
+++ android/src/main/kotlin/me/yohom/amapbase/FunctionRegistry.kt
@@ -7,9 +7,6 @@ import me.yohom.amapbase.map.handler.draw.AddMarkers
 import me.yohom.amapbase.map.handler.draw.AddPolyline
 import me.yohom.amapbase.map.handler.draw.ClearMarker
 import me.yohom.amapbase.map.handler.interact.*
-import me.yohom.amapbase.navi.handler.StartNavi
-import me.yohom.amapbase.search.handler.fetchdata.*
-import me.yohom.amapbase.search.handler.routeplan.CalculateDriveRoute
 
 /**
  * 地图功能集合
@@ -38,20 +35,12 @@ val MAP_METHOD_HANDLER: Map<String, MapMethodHandler> = mapOf(
  * 搜索功能集合
  */
 val SEARCH_METHOD_HANDLER: Map<String, SearchMethodHandler> = mapOf(
-        "search#calculateDriveRoute" to CalculateDriveRoute,
-        "search#searchPoi" to SearchPoiKeyword,
-        "search#searchPoiBound" to SearchPoiBound,
-        "search#searchPoiPolygon" to SearchPoiPolygon,
-        "search#searchPoiId" to SearchPoiId,
-        "search#searchRoutePoiLine" to SearchRoutePoiLine,
-        "search#searchRoutePoiPolygon" to SearchRoutePoiPolygon
 )
 
 /**
  * 导航功能集合
  */
 val NAVI_METHOD_HANDLER: Map<String, NaviMethodHandler> = mapOf(
-        "navi#startNavi" to StartNavi
 )
 
 /**
