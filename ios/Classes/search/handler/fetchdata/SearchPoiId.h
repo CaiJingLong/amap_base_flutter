b3a89e941f4f313a54aa70f26bf9e51763bf0fdc
yohom <382146139@qq.com>
2018-12-13 11:35:52 +0800

    chrore： 漏掉的文件

---
+++ ios/Classes/search/handler/fetchdata/SearchPoiId.h
@@ -0,0 +1,11 @@
+//
+// Created by Yohom Bao on 2018-12-07.
+//
+
+#import <Foundation/Foundation.h>
+#import "AMapSearchKit.h"
+#import "IMethodHandler.h"
+
+
+@interface SearchPoiId : NSObject<SearchMethodHandler, AMapSearchDelegate>
+@end
